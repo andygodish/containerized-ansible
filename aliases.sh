79785d70d5e08faa59b46e39819f12b51764bdda
Andy Godish <33811239+andygodish@users.noreply.github.com>
2026-01-23 12:07:38 -0700

    feat: alias, script, dockerfile to include bootstrapping of ansible p… (#46)
    * feat: alias, script, dockerfile to include bootstrapping of ansible playbook repo
    
    * chore: removed package from build stage, added init-playbook to help function

--- aliases.sh
+++ aliases.sh
@@ -17,9 +17,18 @@ ansible-role-init() {
     docker run -it --rm -v $(pwd):/workspace -w /workspace containerized-ansible:${ANSIBLE_VERSION} ansible-galaxy role init "$1"
 }
 
+ansible-playbook-init() {
+    if [ -z "$1" ]; then
+        echo "Usage: ansible-playbook-init <project-directory>"
+        return 1
+    fi
+    docker run -it --rm -v $(pwd):/workspace -w /workspace containerized-ansible:${ANSIBLE_VERSION} /ansible/scripts/bootstrap-playbook.sh "$1"
+}
+
 # Show available functions
 ansible-help() {
     echo "Available containerized-ansible functions (using version ${ANSIBLE_VERSION}):"
-    echo "  ansible-role-init <role-name>     - Initialize a new Ansible role"
-    echo "  ansible-help                      - Show this help message"
+    echo "  ansible-role-init <role-name>           - Initialize a new Ansible role"
+    echo "  ansible-playbook-init <project-name>    - Initialize a new Ansible playbook project"
+    echo "  ansible-help                            - Show this help message"
 }
